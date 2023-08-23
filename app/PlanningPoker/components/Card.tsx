155e9df909b4e215ac81b085d4ed3b4c4ea1e9aa
f96u <gtfukui@gmail.com>
2023-08-24 08:50:50 +0900

    change: カードのデザインを少し変更する

--- app/PlanningPoker/components/Card.tsx
+++ app/PlanningPoker/components/Card.tsx
@@ -8,6 +8,6 @@ type Props = {
 export default function Card({ children }: Props) {
 
   return (
-    <button className="bg-white rounded-md px-4 py-6 text-lg font-semibold shadow-sm border-blue-500 border">{children}</button>
+    <button className="bg-white rounded-md px-4 py-6 text-lg font-semibold shadow-sm border-blue-500 border hover:bg-blue-50">{children}</button>
   )
 }
