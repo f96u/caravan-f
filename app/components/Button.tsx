f417139a393925f6e1531844358f66fa4a827153
f96u <gtfukui@gmail.com>
2023-09-12 22:50:30 +0900

    change: ボタンのデザインを少し編集

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const Button = ({ onClick, children }: Props) => {
   return (
     <button
-      className="rounded-full border border-purple-200 px-4 py-1 text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+      className="rounded-md border border-purple-200 px-4 py-1 w-fit text-sm font-semibold text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
       onClick={onClick}
     >
       {children}
