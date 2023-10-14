14a30e6baae713a11c5a60fffddaee4f070253ab
f96u <gtfukui@gmail.com>
2023-10-14 12:37:09 +0900

    change: 選択している時に表示が変わるようにする

--- app/planning-poker/[rid]/components/Card.tsx
+++ app/planning-poker/[rid]/components/Card.tsx
@@ -4,12 +4,12 @@ import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
-  selected?: boolean
+  selected: boolean
 }
 export default function Card({ selected, children }: Props) {
   return (
     <div
-      className={`rounded-md w-12 h-20 flex justify-center items-center text-lg font-semibold shadow-sm border-blue-500 border hover:bg-blue-50 ${selected ? 'bg-blue-500' : 'bg-white '}`}
+      className={`rounded-md w-12 h-20 flex justify-center items-center text-lg font-semibold shadow-sm border-indigo-500 border ${selected ? 'bg-indigo-500 text-white' : 'hover:bg-indigo-50 bg-white'}`}
     >
       {children}
     </div>
--- app/planning-poker/[rid]/components/PlayersInfo.tsx
+++ app/planning-poker/[rid]/components/PlayersInfo.tsx
@@ -12,7 +12,7 @@ export const PlayersInfo = ({ players, isTurnOver }: Props) => {
     <div className="flex justify-center items-center m-1 border rounded-md border-gray-500 p-1 [&>:nth-child(n+2)]:ml-4">
       {players ? Object.keys(players).map(player => (
         <div key={player} className="flex flex-col items-center">
-          <Card>{isTurnOver ? players[player].card : "?"}</Card>
+          <Card selected={players[player].card !== 'none'}>{isTurnOver ? players[player].card : "?"}</Card>
           {`${players[player].nickname}[${player.slice(0, 4)}]`}
         </div>
       )) : <div className="h-20 flex items-center"><Spinner /></div>}
