bad5be2e87f6e17ad001ef9d49044566f128059c
f96u <gtfukui@gmail.com>
2023-10-19 09:19:48 +0900

    change: clickableはオプションにする

--- app/planning-poker/[rid]/components/Card.tsx
+++ app/planning-poker/[rid]/components/Card.tsx
@@ -5,9 +5,9 @@ import { ReactNode } from 'react'
 type Props = {
   children: ReactNode
   selected: boolean
-  clickable: boolean
+  clickable?: boolean
 }
-export default function Card({ selected, children, clickable }: Props) {
+export default function Card({ selected, children, clickable = false }: Props) {
   return (
     <div
       className={`rounded-md w-12 h-20 flex justify-center items-center text-lg font-semibold shadow-sm border-indigo-500 border ${clickable ? 'hover:bg-indigo-50' : ''} ${selected ? 'bg-indigo-500 text-white hover:bg-indigo-500' : 'bg-white'}`}
