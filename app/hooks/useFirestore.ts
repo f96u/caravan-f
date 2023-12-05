847e6b352a7e2b59e34efb7e0f8698e41b2def25
f96u <gtfukui@gmail.com>
2023-12-05 09:14:19 +0900

    create: firestoreと接続するhookを作成する

---
+++ app/hooks/useFirestore.ts
@@ -0,0 +1,76 @@
+import {
+  Transaction,
+  Query,
+  runTransaction as originRunTransaction,
+  getDoc as originGetDoc,
+  getDocs as originGetDocs,
+  deleteDoc as originDeleteDoc,
+  addDoc as originAddDoc,
+  DocumentReference, CollectionReference, WithFieldValue
+} from '@firebase/firestore'
+import { db } from '@/app/firebaseApp'
+import { useCallback, useMemo, useState } from 'react'
+
+const BREAK_COUNT = 3
+const LAP_TIME = 3000
+
+/**
+ * firestoreと通信する際にロジック上の考慮が足りてないため書き込みと読み込みがループすることがある。
+ * このループが発生したことを検知して通信を止めるように安全処理を入れたhook
+ */
+export const useFirestore = () => {
+  const [circuitBreak, setCircuitBreak] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const canConnect = useCallback(() => {
+    console.log('CB:' + count)
+    if (count > BREAK_COUNT) {
+      console.error('Circuit breaker')
+      setCircuitBreak(true)
+      return false
+    } else if (circuitBreak) {
+      return false
+    }
+    setCount(prevState => prevState + 1)
+    window.setTimeout(() => setCount(prevState => prevState - 1), LAP_TIME)
+    return true
+  }, [circuitBreak, count])
+
+  const runTransaction = useCallback(((transaction: (transaction: Transaction) => Promise<unknown>) => {
+    if (!canConnect()) {
+      return Promise.reject()
+    }
+
+    return originRunTransaction(db, transaction)
+  }), [canConnect])
+
+  const getDoc = useCallback(<T,>(ref: DocumentReference<T>) => {
+    if (!canConnect()) {
+      return Promise.reject()
+    }
+    return originGetDoc(ref)
+  }, [canConnect])
+
+  const getDocs = useCallback(<T,>(ref: Query<T>) => {
+    if (!canConnect()) {
+      return Promise.reject()
+    }
+    return originGetDocs(ref)
+  }, [canConnect])
+
+  const addDoc = useCallback(<T,>(ref: CollectionReference<T>, data: WithFieldValue<T>) => {
+    if (!canConnect()) {
+      return Promise.reject()
+    }
+    return originAddDoc(ref, data)
+  }, [canConnect])
+
+  const deleteDoc = useCallback((ref: DocumentReference<unknown>) => {
+    if (!canConnect()) {
+      return Promise.reject()
+    }
+    return originDeleteDoc(ref)
+  }, [canConnect])
+
+  return { runTransaction, getDoc, getDocs, addDoc, deleteDoc }
+}
