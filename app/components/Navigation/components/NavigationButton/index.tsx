6d3dcdcd8b6e6e0eaae368080709e88251b76ac2
f96u <gtfukui@gmail.com>
2024-01-27 00:33:51 +0900

    fix: 現在のページいる時のボタン表示ができてなかったので修正

--- app/components/Navigation/components/NavigationButton/index.tsx
+++ app/components/Navigation/components/NavigationButton/index.tsx
@@ -1,18 +1,19 @@
 'use client'
 import { Route } from '@/app/routes'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type Props = {
   route: Route
 }
 
 export const NavigationButton = ({ route }: Props) => {
-  const currentPathname = window.location.pathname
+  const pathname = usePathname()
   return (
     <Link
       href={route.path}
-      className={`rounded-md px-3 py-2 text-sm font-medium ${route.path === currentPathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}
-      { ...(route.path === currentPathname ? { "aria-current": "page" } : {}) }
+      className={`rounded-md px-3 py-2 text-sm font-medium ${route.path === pathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}`}
+      { ...(route.path === pathname ? { "aria-current": "page" } : {}) }
     >
       {route.name}
     </Link>
