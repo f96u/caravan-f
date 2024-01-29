9b635b2a631bed4f83ae27052e4596623f4a6eaf
f96u <gtfukui@gmail.com>
2024-01-29 23:41:45 +0900

    change: CSSを調整してサイト全体のトンマナを合わせる

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const Button = ({ onClick, children, disabled = false, className }: Props) => {
   return (
     <button
-      className={`whitespace-nowrap rounded-md px-3.5 py-2.5 text-sm font-semibold text-white ${disabled ? 'bg-gray-400' : 'bg-indigo-600 hover:bg-indigo-500'} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${className}`}
+      className={`whitespace-nowrap rounded-md px-3.5 py-2.5 text-sm font-semibold text-white ${disabled ? 'bg-gray-400' : 'bg-main hover:bg-sub'} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-1 focus-visible:outline-accent ${className}`}
       disabled={disabled}
       onClick={onClick}
     >
