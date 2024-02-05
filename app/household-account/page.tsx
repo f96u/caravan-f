19693d65c248e4b6e739e68154771aa4dafa69b9
f96u <gtfukui@gmail.com>
2024-02-06 08:47:53 +0900

    add: DatePickerを入れる

--- app/household-account/page.tsx
+++ app/household-account/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { AuthForm } from '@/app/household-account/components/AuthForm'
-import { Dialog } from '@/app/components/Dialog'
+import { DatePicker } from './components/DatePicker'
 
 export default function HouseholdAccount() {
   return (
     <main>
       <h1 className="my-10 text-center text-2xl">複式家計簿</h1>
+      <DatePicker />
     </main>
   )
 }
