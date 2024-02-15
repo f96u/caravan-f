6720636c327cf36c4056291f152f741a4bc23eed
f96u <gtfukui@gmail.com>
2024-02-16 00:07:29 +0900

    add: 月を指定する部分を機能するようにする

--- app/household-account/components/DatePicker/index.tsx
+++ app/household-account/components/DatePicker/index.tsx
@@ -1,4 +1,4 @@
-const Months = () => (
+const Months = ({ year, month }: { year: number; month: number }) => (
   <div className="mx-1.5 grid grid-cols-5 items-center gap-x-3 pb-3">
       <div className="col-span-1">
         <button type="button" className="flex size-8 items-center justify-center rounded-full text-gray-800 hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50">
@@ -7,29 +7,32 @@ const Months = () => (
       </div>
       <div className="col-span-3 flex items-center justify-center gap-x-1">
         <div className="relative">
-          <select>
-            <option value="0">January</option>
-            <option value="1">February</option>
-            <option value="2">March</option>
-            <option value="3">April</option>
-            <option value="4">May</option>
-            <option value="5">June</option>
-            <option value="6" selected>July</option>
-            <option value="7">August</option>
-            <option value="8">September</option>
-            <option value="9">October</option>
-            <option value="10">November</option>
-            <option value="11">December</option>
+          <select data-hs-select='{
+            "placeholder": "Select month",
+            "toggleTag": "<button type=\"button\"></button>",
+            "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative flex text-nowrap w-full cursor-pointer text-start font-medium text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600 before:absolute before:inset-0 before:z-[1]",
+            "dropdownClasses": "mt-2 z-50 w-32 max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300",
+            "optionClasses": "p-2 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100",
+            "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-gray-800\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+          }' className="hidden">
+            {Array.from({ length: 12 }).map((_, idx) => (
+              <option value={idx} selected={idx === month}>{idx + 1}月</option>
+            ))}
           </select>
         </div>
         <span className="text-gray-800">/</span>
         <div className="relative">
-          <select>
-            <option selected>2023</option>
-            <option>2024</option>
-            <option>2025</option>
-            <option>2026</option>
-            <option>2027</option>
+          <select data-hs-select='{
+            "placeholder": "Select year",
+            "toggleTag": "<button type=\"button\"></button>",
+            "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative flex text-nowrap w-full cursor-pointer text-start font-medium text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600 before:absolute before:inset-0 before:z-[1]",
+            "dropdownClasses": "mt-2 z-50 w-20 max-h-72 p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg shadow-lg overflow-hidden overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300",
+            "optionClasses": "p-2 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100",
+            "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 size-3.5 text-gray-800\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
+          }' className="hidden">
+            {Array.from({ length: 3 }).map((_, idx) => (
+              <option selected={year === idx + 2023}>{idx + 2023}</option>
+            ))}
           </select>
         </div>
       </div>
@@ -105,7 +108,7 @@ export const DatePicker = () => {
   return (
     <>
       <div className="grid justify-items-center space-y-0.5">
-        <Months />
+        <Months year={year} month={month} />
         <Weeks />
         <Days year={year} month={month} />
       </div>
