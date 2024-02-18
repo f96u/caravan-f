72ed8ae456601db875f7e055da69cff5be6fcaca
f96u <gtfukui@gmail.com>
2024-02-18 21:09:46 +0900

    change: 日付を選択できるようにする

--- app/household-account/components/DatePicker/components/Days/index.tsx
+++ app/household-account/components/DatePicker/components/Days/index.tsx
@@ -1,16 +1,16 @@
 import { memo } from 'react'
+import { CalendarDate } from '@/app/household-account/components/DatePicker/types/CalendarDate'
 
-/**
- * 指定されたyearとmonthからカレンダーを表示する
- * @param year 年を示す。2024など
- * @param month 月を示す0から11の数字
- * @constructor
- */
-export const Days = memo(function Days({ year, month }: { year: number; month: number}) {
-  const startDay = new Date(year, month, 1).getDay() // 月の最初の日の曜日を取得
-  const endDate = new Date(year, month + 1,  0) // 月の最後の日を取得
-  // 前月の最後の日の情報
-  // 前月の末日
+type Props = {
+  calendarDate: CalendarDate
+  displayingYear: number
+  displayingMonth: number
+  onChange: (date: number) => void
+}
+
+export const Days = memo(function Days({ calendarDate: { year, month, date }, displayingYear, displayingMonth, onChange }: Props) {
+  const startDay = new Date(displayingYear, displayingMonth, 1).getDay() // 月の最初の日の曜日を取得
+  const endDate = new Date(displayingYear, displayingMonth + 1,  0) // 月の最後の日を取得
   const weeks = (weekNum: number ): { day: number, isThisMonth: boolean }[] => Array.from({ length: 7 }).map((_, idx) => {
     // NOTE: 第1周目の処理
     if (weekNum === 0) {
@@ -18,7 +18,7 @@ export const Days = memo(function Days({ year, month }: { year: number; month: n
         return { day: idx -startDay + 1, isThisMonth: true }
       } else {
         // NOTE: 前月の日を表示する
-        return { day: new Date(year, month, 0).getDate() - startDay + idx + 1, isThisMonth: false}
+        return { day: new Date(displayingYear, displayingMonth, 0).getDate() - startDay + idx + 1, isThisMonth: false}
       }
     }
     // NOTE: 第2週目以降の処理
@@ -30,13 +30,21 @@ export const Days = memo(function Days({ year, month }: { year: number; month: n
     return { day: oneDay, isThisMonth: true }
   })
 
+  const isCalendarDate = (day: number) => {
+    return displayingYear === year && displayingMonth === month && day === date
+  }
+
   return (
     <>
       {Array.from({ length: 6 }).map((_, weekNum) => (
         <div key={weekNum} className="flex">
           {weeks(weekNum).map(({ day, isThisMonth }) => (
             <div key={day}>
-              <button disabled={!isThisMonth} type="button" className="m-px flex size-10 items-center justify-center rounded-full border border-transparent text-sm text-gray-800 hover:border-blue-600 hover:text-blue-600 disabled:pointer-events-none disabled:text-gray-300">
+              <button
+                disabled={!isThisMonth}
+                onClick={() => !isCalendarDate(day) && onChange(day)}
+                type="button"
+                className={`m-px flex size-10 items-center justify-center rounded-full border border-transparent text-sm hover:border-blue-600 disabled:pointer-events-none disabled:text-gray-300 ${isCalendarDate(day) && isThisMonth ? 'pointer-events-none bg-blue-600 font-medium text-white': 'text-gray-800 hover:text-blue-600'}`}>
                 {day}
               </button>
             </div>
--- app/household-account/components/DatePicker/index.tsx
+++ app/household-account/components/DatePicker/index.tsx
@@ -1,6 +1,5 @@
-import { MonthPicker } from '@/app/household-account/components/DatePicker/components/MonthPicker'
 import { Days } from '@/app/household-account/components/DatePicker/components/Days'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { CalendarDate } from '@/app/household-account/components/DatePicker/types/CalendarDate'
 
 type Props = {
@@ -9,20 +8,77 @@ type Props = {
 }
 
 export const DatePicker = ({ calendarDate, onChange }: Props) => {
-  const changeMonth = useCallback((year: number, month: number) => onChange({ year, month, date: calendarDate.date }), [calendarDate, onChange])
+  const [displayingYear, setDisplayingYear] = useState(calendarDate.year)
+  const [displayingMonth, setDisplayingMonth] = useState(calendarDate.month)
+  const prevMonth = () => {
+    const { year, month, date } = calendarDate
+    if (month === 0) {
+      setDisplayingYear(prev => prev - 1)
+      setDisplayingMonth(11)
+    } else {
+      setDisplayingMonth(prev => prev - 1)
+    }
+  }
+
+  const nextMonth = () => {
+    const { year, month, date } = calendarDate
+    if (month === 11) {
+      setDisplayingYear(prev => prev + 1)
+      setDisplayingMonth(0)
+    } else {
+      setDisplayingMonth(prev => prev + 1)
+    }
+  }
+
+  const selectYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDisplayingYear(Number(event.target.value))
+  }
+
+  const selectMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setDisplayingMonth(Number(event.target.value))
+  }
+
+  const changeDate = useCallback((date: number) => onChange({ year: displayingYear, month: displayingMonth, date }), [displayingMonth, displayingYear, onChange])
+
   return (
-    <>
-      <div className="grid justify-items-center space-y-0.5">
-        <MonthPicker year={calendarDate.year} month={calendarDate.month} onChange={changeMonth} />
-        <div className="flex pb-1.5">
-          {['日', '月', '火', '水', '木', '金', '土'].map(day => (
-            <span key={day} className="m-px block w-10 text-center text-sm text-gray-500">
-              {day}
-            </span>
-          ))}
+    <div className="grid justify-items-center space-y-0.5">
+      <div className="mx-1.5 grid grid-cols-5 items-center gap-x-3 pb-3">
+        <div className="col-span-1">
+          <button onClick={prevMonth} type="button" className="flex size-8 items-center justify-center rounded-full text-gray-800 hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50">
+            <svg className="size-4 shrink-0" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+          </button>
         </div>
-        <Days year={calendarDate.year} month={calendarDate.month} />
+        <div className="col-span-3 flex items-center justify-center gap-x-1">
+          <div className="relative">
+            <select value={displayingYear} onChange={selectYear} className="h-full border-0 bg-transparent bg-none px-1 focus:ring-0 sm:text-sm">
+              {Array.from({ length: 3 }).map((_, idx) => (
+                <option value={idx + 2023} key={idx}>{idx + 2023}</option>
+              ))}
+            </select>
+          </div>
+          <span className="text-gray-800">/</span>
+          <div className="relative">
+            <select value={displayingMonth} onChange={selectMonth} className="h-full border-0 bg-transparent bg-none px-1 focus:ring-0 sm:text-sm">
+              {Array.from({ length: 12 }).map((_, idx) => (
+                <option key={idx} value={idx}>{idx + 1}月</option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div className="col-span-1 flex justify-end">
+          <button onClick={nextMonth} type="button" className="flex size-8 items-center justify-center rounded-full text-gray-800 hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50">
+            <svg className="size-4 shrink-0" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+          </button>
+        </div>
       </div>
-    </>
+      <div className="flex pb-1.5">
+        {['日', '月', '火', '水', '木', '金', '土'].map(day => (
+          <span key={day} className="m-px block w-10 text-center text-sm text-gray-500">
+            {day}
+          </span>
+        ))}
+      </div>
+      <Days calendarDate={calendarDate} displayingYear={displayingYear} displayingMonth={displayingMonth} onChange={changeDate} />
+    </div>
   )
 }
